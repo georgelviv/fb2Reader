730ee89c30c22c2cbfc6b6e0d232181e64ff2959
georgelviv <lvivgeorge@gmail.com>
2014-11-21 21:12:12 +0200

    simple server with index.html

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var express = require('express'),
 	app = express();
 
 app.get('/', function(req, res) {
-	res.send('Hello World!');
+	res.redirect('/index.html');
 });
+
+app.use(express.static(__dirname + '/dist'));
+
+console.log('Server start at http://localhost:8080/ ...');
 
 app.listen(8080);
 
-console.log('Server start at http://localhost:8080/ ...');
