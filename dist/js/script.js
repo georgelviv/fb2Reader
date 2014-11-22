b2c01af336cd8f0a02f1d59c77e644d41330cec6
georgelviv <lvivgeorge@gmail.com>
2014-11-22 17:45:52 +0200

    uploading file to server

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,16 +1,11 @@
 $(document).ready(function() {
 
-	$('form').submit(function(e) {
-		e.preventDefault();
-		var file = document.getElementById('fileselect').files[0];
-
-		$('form').html('loading...');
-		console.log(file);
-
-		$.post('/upload', function(file) {
-			console.log(file);
-		});
-		
+	$('#fileselect').fileupload({
+		url: '/upload',
+		dataType: 'json',
+			done: function (e, data) {
+			console.log('yes');
+		}
 	});
 
 
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 var express = require('express'),
 	app = express(),
-	fileupload = require('fileupload').createFileUpload('/uploadDir').middleware;
+  upload = require('jquery-file-upload-middleware');
+
+upload.configure({
+  uploadDir: __dirname + '/dist/uploads',
+  uploadUrl: '/uploads',
+});
+
 
 app.get('/', function(req, res) {
-	res.redirect('/index.html');
+	res.redirect('index.html');
 });
 
-app.post('/upload', fileupload, function(req, res) {
-	res.send('e');
-});
+app.post('/upload', upload.fileHandler());
+
 
 app.use(express.static(__dirname + '/dist'));
-
 console.log('Server start at http://localhost:8080/ ...');
-
 app.listen(8080);
 
