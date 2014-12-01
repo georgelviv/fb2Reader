8317f55d25c4c19bb7fa388d81c3c01885201462
garpiya <VeterGarpiya@yandex.ru>
2014-12-02 00:49:17 +0200

    start settingsPanel

---
+++ dev/frontJs/settingsPanel.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+	var fileExtension = /(fb2|epub|txt)/i;
+	var bookName;
+	var getInterval;
+
+	$('#fileselect').fileupload({
+		url: '/upload',
+		dataType: 'json',
+		add: function(e, data) {
+			var format;
+			bookName = data.files[0].name;
+			format = bookName.split('.');
+
+			format = format[format.length - 1];
+			if (format.match(fileExtension)) {
+				data.submit();
+			} else {
+				console.log('error format');
+			}
+		},
+		done: function (e, data) {
+			$('#status').html('File loaded ' + data.result.files[0].name);
+			getInterval = setInterval(getBook, 500);
+			document.body.removeEventListener('keydown', keyPressEvent);
+		},
+		progressall: function(e, data) {
+			var progress = parseInt(data.loaded / data.total * 100, 10);
+			$('#status').html('Loading ' + progress);
+			$('#book').html('');
+		}
+	});
+
+	function getBook() {
+		$.get("/getbook?bookName=" + bookName).done(function(data) {
+			if (data !== 'false') {
+				$('#book').html(data);
+				$('#status').html('Ready ' + bookName);
+				clearInterval(getInterval);
+				document.body.addEventListener('keydown', keyPressEvent);
+			}
+		}).fail(function() {
+			console.log('Error with getting book');
+		});
+		$('#status').html('Parsing ' + bookName);
+	}
+
+	function keyPressEvent(e) {
+		var bookScroll = $('#book')[0].scrollHeight;
+		var bookHeight = $('#book').height();
+		var pages = Math.ceil(bookScroll / bookHeight, bookHeight);
+
+		if (e.keyCode == 39) {
+			$('#book').scrollTop($('#book').scrollTop() + (bookHeight - 10));
+		}
+		if (e.keyCode == 37) {
+			$('#book').scrollTop($('#book').scrollTop() - (bookHeight - 10));
+		}
+	}
+
+});
