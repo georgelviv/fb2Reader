7a2e0aa45a4c92af766836378c463fd155f14537
garpiya <VeterGarpiya@yandex.ru>
2014-12-02 03:39:14 +0200

    began developing settings panel

--- dev/serverJs/txtParser.js
+++ dev/serverJs/txtParser.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var exports = module.exports = {};
 var fs = require('fs');
 var iconv = require('iconv-lite');
@@ -18,27 +17,4 @@ exports.parsingTxt = function(bookName, callback) {
 		exports.txtBook =  bookString.slice(0, -3);
 		callback();
 	});
-
-=======
-var exports = module.exports = {};
-var fs = require('fs');
-var iconv = require('iconv-lite');
-
-exports.parsingTxt = function(bookName, callback) {
-	var bookString = '<p>';
-	
-	fs.readFile('./dist/uploads/' + bookName, function(err, data) {
-		if (err) {
-			console.log('Error with txt. ' + err);
-		}
-
-		bookString =  iconv.decode(data, 'win1251');
-		bookString =  bookString.replace(/\n/g, '</p><p>');
-		bookString =  bookString.replace(/<p>(\n|\s)*<\/p>/g, '');
-
-		exports.txtBook =  bookString.slice(0, -3);
-		callback();
-	});
-
->>>>>>> f0c05408982cc2ba59e9d12952fa7120847dc2c4
 };
